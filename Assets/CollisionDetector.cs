36164aade0889c576bab45ba8710dfd3d5276a6d
Borja <bdesaavazquez@danielcastelao.org>
2026-02-20 11:19:43 +0100

    ejercicio examen

---
+++ Assets/CollisionDetector.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    //cuando empieza la colision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("¡Colisión iniciada! La bola tocó al cubo.");
+        }
+    }
+
+    //mientras esta colisionando
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Colisionando -> (la bola esta tocando el cubo)");
+        }
+    }
+
+    //cuando la colision termina
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("¡Colisión finalizada! La bola se separó del cubo.");
+        }
+    }
+}
